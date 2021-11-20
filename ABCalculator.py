1ae736f9e8882198dd807679fd8174c487c1bdf8
RomanovYO <94523600+RomanovYO@users.noreply.github.com>
2021-11-20 13:23:35 +0200

    Добавление заголовка и кнопок

--- ABCalculator.py
+++ ABCalculator.py
@@ -7,6 +7,24 @@ root = tk.Tk()
 root.geometry('280x300-400+200')
 root.title('A/B калькулятор')
 
+y = 200
+
+# Добавление метки заголовка
+lblTitle = tk.Label(text = 'A/B калькулятор', font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
+lblTitle.place(x = 55, y = 20)
+
+# Добавление кнопки Рассчитать
+y += 50
+btnClose = tk.Button(root, text = 'Рассчитать', font = ('Helvetica', 10, 'bold'))
+btnClose.place(x = 25, y = y, width = 90, height = 30)
+
+# Функция закрытия программы
+def do_close():
+  root.destroy()
+
+# Добавление кнопки закрытия программы
+btnClose = tk.Button(root, text = 'Закрыть', font = ('Helvetica', 10, 'bold'), command = do_close)
+btnClose.place(x = 160, y = y, width = 90, height = 30)
 
 # Запуск цикла mainloop
 root.mainloop()
